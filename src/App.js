6831ad7f969270c5dbc86bdfdcc92a118227ec06
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-04-25 16:30:06 -0300

    upload

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       setTimeout(()=>{
         telaAbertura.style.display = 'none'
       },1000)
-
+      
     },3000)
 
   },[])
@@ -40,7 +40,7 @@ function App() {
       <div className="loadInicial"><img alt="gif de carregamento" src={loadGif} /></div>
 
       <CSSTransition in={location.pathname === '/sign-up'} timeout={300} classNames="page">
-    
+
         <Routes key={location}>
           <Route path="/" element={<Login />} />
           <Route path="sign-up" element={<Cadastrar />} />
