467ca13b14ecca27d9624e4c08d680948f99ea8e
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-04-26 11:28:23 -0300

    upload

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form'
 import ImgMainLogin from '../images/mainIMG-login.png'
 import ImgGmailLogo from '../images/gmail_logo_32px.png'
 import ImgFacebookLogo from '../images/facebook_logo_32px.png'
-    
+
 function Login(){
 
     useEffect(() => {
@@ -28,7 +28,6 @@ function Login(){
             if(data.email === localStorage.getItem('email'+i) && data.senha === localStorage.getItem('senha'+i)){
 
                 loginRealizado = true
-
 
             }
 
